ae13113cfcc1fba52985cb17660f9511908aa429
Kartikeya <kudlu2004@gmail.com>
2023-12-09 18:33:52 +0530

    Added theme

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
       Ecell UVCE
       <ModeToggle />
+      <Button>Some button</Button>
     </main>
   );
 }
