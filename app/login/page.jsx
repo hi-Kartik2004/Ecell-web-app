8c3cc14b676b6740da752db962535a4682b29378
Vansh <vanshayushi@gmail.com>
2023-12-10 13:35:12 +0530

    responsive login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -10,19 +10,19 @@ const page = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         // console.log('Form Values:', { username, password });
-        signInWithEmailAndPassword(auth,username,password).then((userCredential)=>{
+        signInWithEmailAndPassword(auth, username, password).then((userCredential) => {
             console.log(userCredential.user);
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err.code);
         })
     };
 
     return (
-        <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
-            <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
+        <div className="relative p-6 flex flex-col items-center justify-center min-h-screen overflow-hidden">
+            <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl sm:w-11/12">
                 <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-8">
                     <div className="mb-4">
-                        <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">Username:</label>
+                        <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">User Email:</label>
                         <input
                             type="text"
                             id="username"
@@ -43,10 +43,16 @@ const page = () => {
                             autoComplete="off"
                         />
                     </div>
-                    <button type="submit" className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 border">Login</button>
+                    <button
+                        type="submit"
+                        className="w-full lg:w-auto inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 border"
+                    >
+                        Login
+                    </button>
                 </form>
             </div>
         </div>
+
     );
 };
 
