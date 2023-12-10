71cadbe9f5cf89bfb6c93fcf61949af634f7d521
Vansh <vanshayushi@gmail.com>
2023-12-10 13:17:36 +0530

    login button updated

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -43,7 +43,7 @@ const page = () => {
                             autoComplete="off"
                         />
                     </div>
-                    <button type="submit" className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Login</button>
+                    <button type="submit" className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 border">Login</button>
                 </form>
             </div>
         </div>
