3b596e442715f89b62326b54daa93f7b33047f23
Vansh <vanshayushi@gmail.com>
2023-12-14 14:35:36 +0530

    gallery init

---
+++ app/gallery/page.jsx
@@ -0,0 +1,77 @@
+'use client'
+import React, { useState } from 'react';
+
+const images = [
+    'https://placekitten.com/800/600',
+    'https://placekitten.com/600/400',
+    'https://placekitten.com/400/300',
+    'https://placekitten.com/600/400',
+    'https://placekitten.com/400/300',
+];
+
+const Page = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const openModal = (index) => {
+        setSelectedImage(images[index]);
+    };
+
+    const closeModal = () => {
+        setSelectedImage(null);
+    };
+
+    const handleOverlayClick = (e) => {
+        if (e.target.classList.contains('modal-overlay')) {
+            closeModal();
+        }
+    };
+
+    return (
+        <div className="container mx-auto p-4">
+            <br />
+            <br />
+            <br />
+            <h1 className="text-3xl font-bold mb-4">E Cell <span className="text-primary">Gallery</span></h1>
+            <br />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {images.map((image, index) => (
+                    <div
+                        key={index}
+                        className="relative overflow-hidden transition-transform transform hover:scale-105 duration-300 ease-in-out cursor-pointer"
+                        onClick={() => openModal(index)}
+                    >
+                        <img
+                            src={image}
+                            alt={`Image ${index + 1}`}
+                            className="w-full h-full object-cover transition-transform transform-gpu"
+                        />
+                    </div>
+                ))}
+            </div>
+
+            {/* Modal */}
+            {selectedImage && (
+                <div
+                    className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75 modal-overlay"
+                    onClick={handleOverlayClick}
+                >
+                    <div className="max-w-screen-lg w-full mx-auto p-4 relative">
+                        <img
+                            src={selectedImage}
+                            alt="Selected Image"
+                            className="w-full h-full object-contain"
+                        />
+                        <button
+                            onClick={closeModal}
+                            className="absolute top-4 right-4 text-white cursor-pointer bg-gray-800 px-2 py-1 rounded"
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Page;
