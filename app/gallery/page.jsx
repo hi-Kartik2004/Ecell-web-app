df2f29f255c5c432faa5db8726750da78937e5bf
Vansh <vanshayushi@gmail.com>
2023-12-14 15:28:01 +0530

    footer added to gallery

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,5 +1,7 @@
 'use client'
 import React, { useState } from 'react';
+import FooterSection from '../(sections)/FooterSection';
+import { Separator } from "@/components/ui/separator";
 
 const images = [
     'https://placekitten.com/800/600',
@@ -33,49 +35,55 @@ const Page = () => {
     };
 
     return (
-        <div className={`container mx-auto p-4 dark:bg-[url('/texture-pattern.svg')] bg-[url('/texture-pattern-light.svg')]`}>
-            <br />
-            <br />
-            <br />
-            <h1 className={`text-3xl font-bold mb-4 text-white`}>E Cell <span className="text-primary">Gallery</span></h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {images.map((image, index) => (
-                    <div
-                        key={index}
-                        className={`relative overflow-hidden transition-transform transform hover:scale-105 duration-300 ease-in-out cursor-pointer border-2 border-yellow-500 rounded-md`}
-                        onClick={() => openModal(index)}
-                    >
-                        <img
-                            src={image}
-                            alt={`Image ${index + 1}`}
-                            className="w-full h-full object-cover transition-transform transform-gpu rounded"
-                        />
-                    </div>
-                ))}
-            </div>
+        <main>
 
-            {/* Modal */}
-            {selectedImage && (
-                <div
-                    className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75 modal-overlay"
-                    onClick={handleOverlayClick}
-                >
-                    <div className="max-w-screen-lg w-full mx-auto p-4 relative">
-                        <img
-                            src={selectedImage}
-                            alt="Selected Image"
-                            className="w-full h-full object-contain md:object-cover rounded"
-                        />
-                        <button
-                            onClick={closeModal}
-                            className={`absolute top-4 right-4 text-white cursor-pointer ${darkMode ? 'bg-gray-800' : 'bg-gray-200'} px-2 py-1 rounded`}
+            <div className={`container mx-auto p-4 dark:bg-[url('/texture-pattern.svg')] bg-[url('/texture-pattern-light.svg')]`}>
+                <br />
+                <br />
+                <br />
+                <h1 className={`text-3xl font-bold mb-4 text-white`}>E Cell <span className="text-primary">Gallery</span></h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {images.map((image, index) => (
+                        <div
+                            key={index}
+                            className={`relative overflow-hidden transition-transform transform hover:scale-105 duration-300 ease-in-out cursor-pointer border-2 border-yellow-500 rounded-md`}
+                            onClick={() => openModal(index)}
                         >
-                            Close
-                        </button>
-                    </div>
+                            <img
+                                src={image}
+                                alt={`Image ${index + 1}`}
+                                className="w-full h-full object-cover transition-transform transform-gpu rounded"
+                            />
+                        </div>
+                    ))}
                 </div>
-            )}
-        </div>
+
+                {/* Modal */}
+                {selectedImage && (
+                    <div
+                        className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75 modal-overlay"
+                        onClick={handleOverlayClick}
+                    >
+                        <div className="max-w-screen-lg w-full mx-auto p-4 relative">
+                            <img
+                                src={selectedImage}
+                                alt="Selected Image"
+                                className="w-full h-full object-contain md:object-cover rounded"
+                            />
+                            <button
+                                onClick={closeModal}
+                                className={`absolute top-4 right-4 text-white cursor-pointer ${darkMode ? 'bg-gray-800' : 'bg-gray-200'} px-2 py-1 rounded`}
+                            >
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </div>
+            <Separator />
+            <br />
+            <FooterSection />
+        </main>
     );
 };
 
