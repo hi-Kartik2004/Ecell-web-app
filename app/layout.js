a738c574b74bc6ab597b72b9b7f54afbe0e6491a
Kartikeya <kudlu2004@gmail.com>
2023-12-23 15:57:57 +0530

    Changed default theme

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
