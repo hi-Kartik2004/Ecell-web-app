76b6503b56020fd334b1511c2657a6a4225e24ad
Kartikeya <kudlu2004@gmail.com>
2023-12-23 16:30:03 +0530

    Footer changes

--- components/FooterLinksGroup.jsx
+++ components/FooterLinksGroup.jsx
@@ -7,29 +7,31 @@ function FooterLinksGroup() {
   return (
     <>
       <div className="flex flex-col items-center">
-        <h3 className="text-lg mb-2 text-primary">Lorem, ipsum dolor.</h3>
+        <h3 className="text-lg mb-2 dark:text-primary font-medium text-muted-foreground">
+          Lorem, ipsum dolor.
+        </h3>
         <div className="mt-2 flex flex-col items-start gap-3">
           <Link
             href="/"
-            className="text-sm text-muted-foreground hover:text-primary hover:underline underline-offset-4"
+            className="text-sm text-muted-foreground hover:text-black dark:hover:text-primary hover:underline underline-offset-4"
           >
             Lorem, ipsum.
           </Link>
           <Link
             href="/"
-            className="text-sm text-muted-foreground hover:text-primary hover:underline underline-offset-4"
+            className="text-sm text-muted-foreground hover:text-black dark:hover:text-primary hover:underline underline-offset-4"
           >
             Lorem, ipsum.
           </Link>
           <Link
             href="/"
-            className="text-sm text-muted-foreground hover:text-primary hover:underline underline-offset-4"
+            className="text-sm text-muted-foreground hover:text-black dark:hover:text-primary hover:underline underline-offset-4"
           >
             Lorem, ipsum.
           </Link>
           <Link
             href="/"
-            className="text-sm text-muted-foreground hover:text-primary hover:underline underline-offset-4"
+            className="text-sm text-muted-foreground hover:text-black dark:hover:text-primary hover:underline underline-offset-4"
           >
             Lorem, ipsum.
           </Link>
