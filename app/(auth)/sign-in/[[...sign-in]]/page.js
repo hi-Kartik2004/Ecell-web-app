689c97250a66715808c19d9046b5ce3b10b9b8e1
Kartikeya <kudlu2004@gmail.com>
2023-12-23 21:59:26 +0530

    Added card loader

--- app/(auth)/sign-in/[[...sign-in]]/page.js
+++ app/(auth)/sign-in/[[...sign-in]]/page.js
@@ -1,9 +1,13 @@
-import { SignIn } from "@clerk/nextjs";
+import { Skeleton } from "@/components/ui/skeleton";
+import { ClerkLoading, SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <>
       <section className="h-[100vh] flex items-center justify-center dark:bg-[url('/texture-pattern.svg')] bg-[url('/texture-pattern-light.svg')]">
+        <ClerkLoading>
+          <Skeleton className={"w-[400px] h-[500px]"} />
+        </ClerkLoading>
         <SignIn />
       </section>
     </>
