df2ed5512b187f7a508df546847a5bbf0bcc2c68
Vansh <vanshayushi@gmail.com>
2023-12-25 02:17:45 +0530

    server done

--- server/routes/subscribers.js
+++ server/routes/subscribers.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Subscriber = require('../model/subscriber')
 
 
-// getting all
+// all
 router.get('/', async (req, res) => {
     try {
         const subscribers = await Subscriber.find()
@@ -12,5 +12,26 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: err.message })
     }
 })
+
+// one
+router.get('/:email', getSubscriber, (req, res) => {
+    res.json(res.subscriber)
+})
+
+
+async function getSubscriber(req, res, next) {
+    let subscriber
+    try {
+        subscriber = await Subscriber.findOne({email:req.params.email})
+        if (subscriber == null) {
+            return res.status(404).json({ message: 'Cannot find' })
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+
+    res.subscriber = subscriber
+    next()
+}
 
 module.exports = router
