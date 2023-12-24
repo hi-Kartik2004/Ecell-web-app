65af852382d859ee37c844ac8c0888884de8b897
Vansh <vanshayushi@gmail.com>
2023-12-25 02:02:29 +0530

    schema setup

--- server/routes/subscribers.js
+++ server/routes/subscribers.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require('../model/subscriber')
 
 
 // getting all
-router.get('/',(req, res) => {
-    res.send("hello world")
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
 
 module.exports = router
