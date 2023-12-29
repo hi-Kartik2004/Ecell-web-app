a58528fe10f9cb4e0302d4041fdfc24fa9fc0630
Kartikeya <kudlu2004@gmail.com>
2023-12-29 13:37:40 +0530

    Foot bg changed

--- components/Foot.jsx
+++ components/Foot.jsx
@@ -5,7 +5,7 @@ function Foot() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="dark:bg-[url('/texture-pattern.svg')] bg-[url('/texture-pattern-light.svg')] p-2 text-center">
+    <div className="bg-card p-2 text-center">
       <span className="text-xs md:text-sm">
         &copy; {currentYear} {data?.footText}
       </span>
