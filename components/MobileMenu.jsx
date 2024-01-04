985da3756510296a671b822af52883af8d7d2b17
Kartikeya <kudlu2004@gmail.com>
2024-01-04 16:16:04 +0530

    Modified the Mobile menu

--- components/MobileMenu.jsx
+++ components/MobileMenu.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
@@ -15,24 +16,17 @@ import data from "@/app/data";
 import Link from "next/link";
 
 function MobileMenu() {
-  const [showSlider, setShowSlider] = useState(false);
-
   return (
-    <Sheet open={showSlider}>
+    <Sheet>
       <SheetTrigger asChild>
-        <Button
-          variant="muted"
-          onClick={() => {
-            setShowSlider(!showSlider);
-          }}
-        >
+        <Button variant="muted">
           <HamburgerMenuIcon className="w-5 h-5" />
         </Button>
       </SheetTrigger>
       <SheetContent className={`max-w-[250px] px-1 `} side="left">
         <SheetHeader>
-          <SheetTitle>{data?.clubName} Menu</SheetTitle>
-          <SheetDescription className="text-xs">
+          <SheetTitle className="text-center">{data?.clubName} Menu</SheetTitle>
+          <SheetDescription className="text-xs text-center">
             {data?.mobileMenuDescription}
           </SheetDescription>
         </SheetHeader>
@@ -40,20 +34,19 @@ function MobileMenu() {
         <div className="flex flex-col gap-4 items-center my-10 text-center">
           <div className="flex gap-10 flex-col md:flex-row">
             {data.navbarLinks.map((link, index) => (
-              <Link
-                href={link.link}
-                className="opacity-75 text-xs hover:opacity-100 ease-in-out duration-100 hover:underline underline-offset-4"
-                onClick={() => {
-                  setShowSlider(false);
-                }}
-              >
-                {link.name}
-              </Link>
+              <SheetClose key={link} asChild>
+                <Link
+                  href={link.link}
+                  className="opacity-75 text-xs hover:opacity-100 ease-in-out duration-100 hover:underline underline-offset-4"
+                >
+                  {link.name}
+                </Link>
+              </SheetClose>
             ))}
           </div>
         </div>
 
-        <div className="absolute bottom-4 flex flex-wrap justify-center mx-2">
+        <div className="absolute w-full bottom-4 flex flex-wrap justify-center items-center">
           <p className="text-xs text-center flex-wrap flex">
             &copy; {new Date().getFullYear()} {data?.footText}.
           </p>
